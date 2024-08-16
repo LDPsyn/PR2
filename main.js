c3fdbc5e11cc88ad878f3c587df303ce30767150
LDPsyn <124814608+LDPsyn@users.noreply.github.com>
2024-08-16 09:02:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -46,3 +46,29 @@ function changeImage() {
     currentImageIndex = (currentImageIndex + 1) % imagePaths.length;
     imageElement.src = imagePaths[currentImageIndex];
 }
+// Función para refrescar la página sin mostrar el login
+function refreshWithoutLogin() {
+    // Obtener la URL actual
+    var currentUrl = window.location.href;
+  
+    // Verificar si la URL contiene el token de autenticación
+    if (currentUrl.includes('?token=')) {
+      // Dividir la URL en partes
+      var urlParts = currentUrl.split('?');
+  
+      // Obtener la parte de la URL después del token
+      var urlWithoutToken = urlParts[0];
+  
+      // Agregar el token a la nueva URL
+      var newUrl = urlWithoutToken + '?' + urlParts[1];
+  
+      // Recargar la página con la nueva URL
+      window.location.replace(newUrl);
+    } else {
+      // Si no hay token, recargar la página normalmente
+      window.location.reload();
+    }
+  }
+  
+  // Llamar a la función refreshWithoutLogin después de 5 horas (18000 segundos)
+  setTimeout(refreshWithoutLogin, 18000000);
