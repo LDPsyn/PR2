72e18624fe5ea2829d22c993bdeac9c3e1e52dfc
LDPsyn <124814608+LDPsyn@users.noreply.github.com>
2024-08-19 15:07:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,51 +1,23 @@
-// Elementos del DOM
-const loginPopup = document.getElementById('login-popup');
-const closeBtn = document.querySelector('.close-btn');
-const loginBtn = document.getElementById('login-btn');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-const imageContainer = document.getElementById('image-container');
-const imageElement = document.getElementById('image');
-
-// Credenciales de acceso (puedes cambiarlas)
-const validUsername = 'admin';
-const validPassword = 'contraseña';
-
 // Rutas de las imágenes
 const imagePaths = ['imagen1.png', 'imagen2.png'];
 let currentImageIndex = 0;
 
-// Mostrar el popup de inicio de sesión
-window.onload = function() {
-    loginPopup.style.display = 'block';
-}
+// Elementos del DOM
+const imageContainer = document.getElementById('image-container');
+const imageElement = document.getElementById('image');
 
-// Cerrar el popup de inicio de sesión
-closeBtn.onclick = function() {
-    loginPopup.style.display = 'none';
-}
+// Mostrar el contenedor de la imagen
+imageContainer.style.display = 'flex';
 
-// Manejar el evento de clic en el botón de inicio de sesión
-loginBtn.addEventListener('click', () => {
-    if (username.value === validUsername && password.value === validPassword) {
-        // Ocultar el popup de inicio de sesión
-        loginPopup.style.display = 'none';
-
-        // Mostrar el contenedor de la imagen
-        imageContainer.style.display = 'flex';
-
-        // Iniciar el cambio de imágenes
-        setInterval(changeImage, 20000);
-    } else {
-        alert('Usuario o contraseña incorrectos');
-    }
-});
+// Iniciar el cambio de imágenes
+setInterval(changeImage, 20000);
 
 // Función para cambiar la imagen
 function changeImage() {
     currentImageIndex = (currentImageIndex + 1) % imagePaths.length;
     imageElement.src = imagePaths[currentImageIndex];
 }
+
 // Función para refrescar la página sin mostrar el login
 function refreshWithoutLogin() {
     // Obtener la URL actual
